f45490f894483a8e99d636ce275c93804e683a2c
MACHE022 <edson.machado.-nd@disney.com>
2021-10-11 18:04:20 +0200

    How to use the Fetch API

--- fetch-api-lab/app/js/main.js
+++ fetch-api-lab/app/js/main.js
@@ -21,51 +21,111 @@ function logResult(result) {
 }
 
 function logError(error) {
-  console.log('Looks like there was a problem:', error);
+  console.log("Looks like there was a problem:", error);
 }
 
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+function readResponseAsJSON(response) {
+  return response.json();
+}
+
+function showImage(responseAsBlob) {
+  const container = document.getElementById("img-container");
+  const imgElem = document.createElement("img");
+  container.appendChild(imgElem);
+  const imgUrl = URL.createObjectURL(responseAsBlob);
+  imgElem.src = imgUrl;
+}
+
+function readResponseAsBlob(response) {
+  return response.blob();
+}
+
+function readResponseAsText(response) {
+  return response.text();
+}
+
+function showText(responseAsText) {
+  const message = document.getElementById("message");
+  message.textContent = responseAsText;
+}
 
 // Fetch JSON ----------
 
 function fetchJSON() {
-  // TODO
+  fetch("examples/animals.json") // 1
+    .then(validateResponse) // 2
+    .then(readResponseAsJSON) // 3
+    .then(logResult) // 4
+    .catch(logError);
 }
-const jsonButton = document.getElementById('json-btn');
-jsonButton.addEventListener('click', fetchJSON);
-
+const jsonButton = document.getElementById("json-btn");
+jsonButton.addEventListener("click", fetchJSON);
 
 // Fetch Image ----------
 
 function fetchImage() {
-  // TODO
+  fetch("examples/fetching.jpg")
+    .then(validateResponse)
+    .then(readResponseAsBlob)
+    .then(showImage)
+    .catch(logError);
 }
-const imgButton = document.getElementById('img-btn');
-imgButton.addEventListener('click', fetchImage);
-
+const imgButton = document.getElementById("img-btn");
+imgButton.addEventListener("click", fetchImage);
 
 // Fetch text ----------
 
 function fetchText() {
-  // TODO
+  fetch("examples/words.txt")
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(showText)
+    .catch(logError);
 }
-const textButton = document.getElementById('text-btn');
-textButton.addEventListener('click', fetchText);
-
+const textButton = document.getElementById("text-btn");
+textButton.addEventListener("click", fetchText);
 
 // HEAD request ----------
 
 function headRequest() {
-  // TODO
+  fetch("examples/words.txt", {
+    method: "HEAD"
+  })
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(logResult)
+    .catch(logError);
 }
-const headButton = document.getElementById('head-btn');
-headButton.addEventListener('click', headRequest);
-
+const headButton = document.getElementById("head-btn");
+headButton.addEventListener("click", headRequest);
 
 // POST request ----------
 
-/* NOTE: Never send unencrypted user credentials in production! */
 function postRequest() {
-  // TODO
+  const formData = new FormData(document.getElementById("msg-form"));
+  const messageHeaders = new Headers({
+    "Content-Type": "application/json",
+    // 'Content-Length': 'kittens' // Content-Length can't be modified!
+    "X-Custom": "hello world"
+    // "Y-Custom": "this will not work" // Y-Custom is not accepted by our echo server!
+  });
+  fetch("http://localhost:5000/", {
+    method: "POST",
+    // body: formData,
+    body: JSON.stringify({ lab: "fetch", status: "fun" }),
+    headers: messageHeaders
+  })
+    .then(validateResponse)
+    .then(readResponseAsText)
+    .then(showText)
+    .catch(logError);
 }
-const postButton = document.getElementById('post-btn');
-postButton.addEventListener('click', postRequest);
+const postButton = document.getElementById("post-btn");
+postButton.addEventListener("click", postRequest);
