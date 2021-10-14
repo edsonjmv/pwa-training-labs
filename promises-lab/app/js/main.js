678ffefc88400b25c53d57352d735bdefafc487f
MACHE022 <edson.machado.-nd@disney.com>
2021-10-14 17:41:55 +0200

    Basics of JavaScript Promises!

--- promises-lab/app/js/main.js
+++ promises-lab/app/js/main.js
@@ -16,46 +16,77 @@ limitations under the License.
 /*jshint esversion: 6*/
 
 const app = (() => {
-
   function getImageName(country) {
-
-    // create and return a promise
-
+    country = country.toLowerCase();
+    const promiseOfImageName = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (country === "spain" || country === "chile" || country === "peru") {
+          resolve(country + ".png");
+        } else {
+          reject(Error("Didn't receive a valid country name!"));
+        }
+      }, 1000);
+    });
+    console.log(promiseOfImageName);
+    return promiseOfImageName;
   }
 
   function isSpain(country) {
-
-    // Optional - create and return a promise that resolves if input is "Spain"
-
+    country = country.toLowerCase();
+    if (country === "spain") {
+      resolve(true);
+    } else {
+      reject(Error(false));
+    }
   }
 
   function flagChain(country) {
-
-    // use the promise
-
+    return getImageName(country)
+      .then(fetchFlag)
+      .then(processFlag)
+      .then(appendFlag)
+      .catch(logError);
   }
 
   function allFlags(promiseList) {
-
-    // use promise.all
-
+    return Promise.all(promiseList)
+      .then((result) => {
+        console.log("Flags loaded");
+        return result;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
+  var promises = [
+    getImageName("Spain"),
+    getImageName("Chile"),
+    getImageName("Peru")
+  ];
 
-  // call the allFlags function
+  allFlags(promises).then((result) => {
+    console.log(result);
+  });
 
+  const promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, "one");
+  });
 
-  // use Promise.race
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, "two");
+  });
 
+  Promise.race([promise1, promise2]).then(logSuccess).catch(logError);
 
   /* Helper functions */
 
   function logSuccess(result) {
-    console.log('Success!:\n' + result);
+    console.log("Success!:\n" + result);
   }
 
   function logError(err) {
-    console.log('Oh no!:\n' + err);
+    console.log("Oh no!:\n" + err);
   }
 
   function returnFalse() {
@@ -63,40 +94,39 @@ const app = (() => {
   }
 
   function fetchFlag(imageName) {
-    return fetch('flags/' + imageName); // fetch returns a promise
+    return fetch("flags/" + imageName); // fetch returns a promise
   }
 
   function processFlag(flagResponse) {
     if (!flagResponse.ok) {
-      throw Error('Bad response for flag request!'); // This will implicitly reject
+      throw Error("Bad response for flag request!"); // This will implicitly reject
     }
     return flagResponse.blob(); // blob() returns a promise
   }
 
   function appendFlag(flagBlob) {
-    const flagImage = document.createElement('img');
+    const flagImage = document.createElement("img");
     const flagDataURL = URL.createObjectURL(flagBlob);
     flagImage.src = flagDataURL;
-    const imgContainer = document.getElementById('img-container');
+    const imgContainer = document.getElementById("img-container");
     imgContainer.appendChild(flagImage);
-    imgContainer.style.visibility = 'visible';
+    imgContainer.style.visibility = "visible";
   }
 
   function fallbackName() {
-    return 'chile.png';
+    return "chile.png";
   }
 
   // Don't worry if you don't understand this, it's not part of Promises.
   // We are using the JavaScript Module Pattern to enable unit testing of
   // our functions.
   return {
-    getImageName: (getImageName),
-    flagChain: (flagChain),
-    isSpain: (isSpain),
-    fetchFlag: (fetchFlag),
-    processFlag: (processFlag),
-    appendFlag: (appendFlag),
-    allFlags: (allFlags)
+    getImageName: getImageName,
+    flagChain: flagChain,
+    isSpain: isSpain,
+    fetchFlag: fetchFlag,
+    processFlag: processFlag,
+    appendFlag: appendFlag,
+    allFlags: allFlags
   };
-
 })();
