b6aa8c52383987478dacdae090c79bcb2cbb46eb
MACHE022 <edson.machado.-nd@disney.com>
2021-11-01 16:20:27 +0100

    How to set up gulp, create tasks using plugins, and automate development

---
+++ gulp-lab/project/gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+function copy() {
+  return gulp
+    .src(['app/*.html', 'app/**/*.jpg', 'app/**/*.css'])
+    .pipe(gulp.dest('build'));
+}
+
+gulp.task('copy', copy);
+
+function serve() {
+  return browserSync.init({
+    server: 'build',
+    open: false,
+    port: 3000
+  });
+}
+
+gulp.task('buildAndServe', gulp.series(copy, serve));
+
+function processJs() {
+  return gulp
+    .src('app/scripts/*.js')
+    .pipe(
+      babel({
+        presets: ['env']
+      })
+    )
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(gulp.dest('build/scripts'));
+}
+
+gulp.task('processJs', processJs);
+
+function watch() {
+  gulp.watch('app/scripts/*.js', processJs);
+}
+
+gulp.task('watch', watch);
