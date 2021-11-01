58b3d139dd2c016bd8c0b0aece6a32572f1addb0
MACHE022 <edson.machado.-nd@disney.com>
2021-11-01 15:52:54 +0100

    How to implement the basics of offline functionality

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,32 +1,30 @@
-
-const cacheName = 'cache-v1';
+const cacheName = "cache-v1";
 const precacheResources = [
-  '/',
-  'index.html',
-  'styles/main.css',
-  'images/space1.jpg',
-  'images/space2.jpg',
-  'images/space3.jpg'
+  "/",
+  "index.html",
+  "styles/main.css",
+  "images/space1.jpg",
+  "images/space2.jpg",
+  "images/space3.jpg"
 ];
 
-self.addEventListener('install', event => {
-  console.log('Service worker install event!');
+self.addEventListener("install", (event) => {
+  console.log("Service worker install event!");
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(precacheResources);
-      })
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(precacheResources);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service worker activate event!');
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activate event!");
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(caches.match(event.request)
-    .then(cachedResponse => {
+self.addEventListener("fetch", (event) => {
+  console.log("Fetch intercepted for:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) {
         return cachedResponse;
       }
